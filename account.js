e0c59c4b8badd71ac58fad5e5a1c10cafc9b6a23
Zjicm·YW <zeroology@foxmail.com>
2020-05-24 00:46:05 +0800

    add actionHistory

--- account.js
+++ account.js
@@ -1,14 +1,4 @@
-const { Api, JsonRpc, RpcError } = require('eosjs');
-const JsSignatureProvider = require('eosjs/dist/eosjs-jssig');  // development only
-const fetch = require('node-fetch');                            // node only; not needed in browsers
-const { TextEncoder, TextDecoder } = require('util');           // node only; native TextEncoder/Decoder 
-const config=require('./config.json')
-const address = config['account']['address']
-const signatureProvider = new JsSignatureProvider.JsSignatureProvider([config['account']['privateKey']]);
-const rpc = new JsonRpc(config['eosapi'], { fetch });
-const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
-
-
+const {rpc,address} = require('./eosAPI');
 
 // rpc对象支持promise，所以使用 async/await 函数运行rpc命令
 const  runRpc = async () => {
@@ -19,11 +9,15 @@ const  runRpc = async () => {
 
   // 获取账号的信息
   const accountInfo = await rpc.get_account(address);
-  console.log(accountInfo);
+  // console.log(accountInfo);
 
   //获取账号的资产
   // const balance = await rpc.get_currency_balance('eosio.token',address);
   // console.log(balance);
+
+  //获取账号操作历史
+  const actionHistory = await rpc.history_get_actions(address);
+  console.log(actionHistory);
 
 };
 
